760ed8ebcd95b0867656fe3581a13ee7ce5a1e0d
Daniel User <daniel.sukhovilin@gmail.com>
2022-03-25 15:13:43 +0300

    name

--- src/main/java/com/daniel/mysuperchat/App.java
+++ src/main/java/com/daniel/mysuperchat/App.java
@@ -1,4 +1,12 @@
 package com.daniel.mysuperchat;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("your name: ");
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
 }
