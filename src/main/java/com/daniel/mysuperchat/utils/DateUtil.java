65b03a00c64af6feb1c72b11a1c91276df3ec456
dmitres <dmitresoft@gmail.com>
2022-03-27 20:20:18 +0300

    formatting date

--- src/main/java/com/daniel/mysuperchat/utils/DateUtil.java
+++ src/main/java/com/daniel/mysuperchat/utils/DateUtil.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class DateUtil {
 
-    public static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm");
+    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm");
 
     public static String format(Date date) {
         return FORMAT.format(date);
